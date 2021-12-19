d5ff3b2ac3627380c0db5492e7d3b5f629e6c678
VINEET KUMAR SHARMA <vineetkumarsharma17@gmail.com>
2021-12-19 13:17:36 +0530

    first commit

---
+++ db_config.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "pal_database";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+?>
---
+++ excelUpload4.php
@@ -0,0 +1,40 @@
+<?php  
+include("db_config.php"); 
+$uploaddir = 'uploads/';
+$uploadfile = $uploaddir . basename($_FILES['file']['name']);
+$title = $_POST["title"];
+$sql[] = array();
+if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile)) {
+$delete= mysqli_query($conn,"DROP TABLE data4");
+// die();
+$handle = fopen($uploadfile, "r");
+$header = fgetcsv($handle,1000,",");
+if($header){
+    $header_sql = array();
+    foreach($header as $h){
+        $header_sql[] = '`'.$h.'` VARCHAR(255)';
+    }
+    $sql[] = 'CREATE TABLE `data4` ('.implode(',',$header_sql).')';
+    // $sql[] = 'ALTER TABLE `data2` ADD `Mahindra` VARCHAR(255)';
+    while($data = fgetcsv($handle,1000,",")){   
+        $sql[] = "INSERT INTO data4 VALUES ('".implode("','",$data)."')";
+    }
+     $sql[] = "ALTER TABLE `data4` ADD $title VARCHAR(255)";
+}        
+foreach($sql as $s){
+  if($s!=null)
+    mysqli_query($conn,$s);
+}
+
+echo '<script>alert("Successfully uploaded")</script>';
+// session_start();
+// $_SESSION["status_success"]= true;
+}else
+echo '<script>alert("Not uploaded")</script>';
+// $_SESSION["status_failed"]=false;
+// header("Location: http://localhost/balajirepo/index.php");
+// exit;
+// echo 'Here is some more debugging info:';
+// print_r($_FILES);
+// print "</pre>";
+?>
